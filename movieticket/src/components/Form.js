d36771212f3e71bcd26e552a524c2600e08fe887
CMU\rungroj_s <rungroj029@gmail.com>
2018-05-31 11:14:37 +0700

    Now can binding data from select dropdown

--- movieticket/src/components/Form.js
+++ movieticket/src/components/Form.js
@@ -1,17 +1,54 @@
 import React from "react";
 
 class Form extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            price:undefined,
+            movieDataID: undefined,
+            movieDataName: undefined,
+            movieDataPrice: undefined,
+            movieDataImage: undefined,
+            movieDataNowShowing: undefined,
+            };
+        
+     this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange = (e) => {
+   
+        var valueData = this.props.values.filter(function(item) {
+            return item.name === e.target.value
+          })
+        this.setState({
+             movieDataID: valueData[0].id,
+            movieDataName: valueData[0].name,
+            movieDataPrice: valueData[0].price,
+            movieDataImage: valueData[0].image,
+            movieDataNowShowing: valueData[0].now_showing,
+            
+        })
+        e.preventDefault();
+      }
 
 render(){
     return (
+        <div>
         <form >
-            <select>
-                { this.props.values.map((movie, key) => <option key={key} >{movie.name}</option>) }
-            </select>
-            <button>Submit</button>
+        <select onChange={this.handleChange}>
+        {this.props.values.map(function(data, key){  return (
+          <option key={key} value={data.name}>{data.name}</option> )
+        })}
+        </select>
+            {/* <button >Submit</button> */}
         </form>
-    
+      
         
+
+      </div>
+    
+//     <input type = "number" value = {this.state.movieData.price} 
+//     onChange = {this.updateState} />
+//  <h3>{this.state.data}</h3>
     )
 }
 }
